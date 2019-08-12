7de9f302bd1505eaff4354c3a9e8a437c8402b2a
Ege Uz <enkomaun@gmail.com>
2019-08-12 17:46:27 -0400

    lines completed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const seed = Math.random();
+
 function setup() {
   const canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('drawingCanvas');
@@ -13,43 +15,53 @@ function draw() {
   stroke(255);
   strokeWeight(3);
 
-  //Watch for window resizes!
-  let screenIsWide = windowWidth > 800;
+  let screenIsWide = windowWidth > 800; //Watch for window resizes
+  let radius = (screenIsWide) ? 240 : (windowWidth * 0.3); //Resize polygon radius based on width
 
-  //Create base hexagon
   push();
   noFill();
-  let radius = (screenIsWide) ? 240 : (windowWidth * 0.3);
   polygon(windowWidth/2, windowHeight/2, radius, 6);
   pop();
-  
-  //dynamically define hexagon points
-  // let firstLineX = windowWidth * 0.4;
-  // let firstLineY = windowHeight * 0.5;
 
-  let horizontalMidLine = radius * 2;
-  let verticalMidLine = horizontalMidLine * 0.87;
-
-  //geometry variables
-  let x0 = (windowWidth - horizontalMidLine) / 2
-  let x1 = ((windowWidth - horizontalMidLine) / 2) + (horizontalMidLine / 4);
-  let x2 = ((windowWidth - horizontalMidLine) / 2) + (horizontalMidLine * 0.75);
-  let x3 = (windowWidth + horizontalMidLine) / 2
-
-  let y0 = (windowHeight - verticalMidLine) / 2;
-  let y1 = windowHeight / 2;
-  let y2 = (windowHeight + verticalMidLine) / 2;
-
-  noFill();
-
-  line(x0, y1, mouseX, mouseY);
-  line(x1, y0, mouseX, mouseY);
-  line(x2, y0, mouseX, mouseY);
-  line(x3, y1, mouseX, mouseY);
-  line(x1, y2, mouseX, mouseY);
-  line(x2, y2, mouseX, mouseY);
-  
+  //Generate lines
+  generateLines(radius);
 }
+
+//Line generation function
+function generateLines(radius) {
+
+    //Geometric variables
+    let horizontalMidLine = radius * 2;
+    let verticalMidLine = horizontalMidLine * 0.87;
+
+    //Coordinates
+    const x0 = (windowWidth - horizontalMidLine) / 2
+    const x1 = ((windowWidth - horizontalMidLine) / 2) + (horizontalMidLine / 4);
+    const x2 = ((windowWidth - horizontalMidLine) / 2) + (horizontalMidLine * 0.75);
+    const x3 = (windowWidth + horizontalMidLine) / 2
+  
+    const y0 = (windowHeight - verticalMidLine) / 2 + 1;
+    const y1 = windowHeight / 2;
+    const y2 = (windowHeight + verticalMidLine) / 2 - 1;
+
+
+    if(seed > 0.5) {
+      //Distribution form 1
+      line(x0, y1, mouseX, mouseY);
+      line(x2, y0, mouseX, mouseY);
+      line(x2, y2, mouseX, mouseY);
+    } else {
+      //Distribution form 2
+      line(x3, y1, mouseX, mouseY);
+      line(x1, y0, mouseX, mouseY);
+      line(x1, y2, mouseX, mouseY);
+    }
+
+
+
+
+}
+
 
 //Polygon base function
 function polygon(x, y, radius, npoints) {
